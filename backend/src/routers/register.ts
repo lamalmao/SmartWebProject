eb6393672c193bd42ffb3e9335633277194d9e1a
lamalmao <laesia>
2023-03-17 21:18:57 +0300

    Заготовка для маргшрута регистрации

---
+++ backend/src/routers/register.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const signupRouter = express.Router();
+
+// signupRouter.use((req, res) => {
+//   try {
+    
+//   } catch (e) {
+    
+//   }
+// });
+
+export default signupRouter;
