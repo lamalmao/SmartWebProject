1d0a2b826f27e03ed45f61bd4e98153ff35428be
Trymedvedya <106478028+Trymedvedya@users.noreply.github.com>
2023-03-18 17:02:53 +0300

    Закинул модалку на кнопку войти

--- frontend/src/React-components/Components/Navbar/Navbar.jsx
+++ frontend/src/React-components/Components/Navbar/Navbar.jsx
@@ -4,13 +4,13 @@ import { useSelector,useDispatch } from 'react-redux';
 import {visible} from '../../Redux-slices/visibleSlice'
 import ModalWindow from '../../UI-Components/ModalWindow/ModalWindow'
 import QuizForm from '../QuizForm/QuizForm';
-
+import TgForm from '../TgForm/TgForm'
 const Navbar = () => {
     useSelector((state)=>state.visibility.value);
     const dispatch = useDispatch();
     return (
         <header>
-                <ModalWindow><QuizForm></QuizForm></ModalWindow>
+                <ModalWindow><TgForm></TgForm></ModalWindow>
             <div>
                 <h2 className={st.Logo}><span className={st.LogoBold}>GEO</span>GAP</h2>
             </div>
