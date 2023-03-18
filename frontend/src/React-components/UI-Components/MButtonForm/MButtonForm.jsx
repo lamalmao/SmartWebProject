c4411a30531c0defa2776537a5f01edfedbfd3d1
den4day <den4day@gmail.com>
2023-03-18 13:22:14 +0300

    fix

--- frontend/src/React-components/UI-Components/MButtonForm/MButtonForm.jsx
+++ frontend/src/React-components/UI-Components/MButtonForm/MButtonForm.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import st from './MButtonForm.module.css'
 
-const MButtonForm = ({surs,children, ...props}) => {
+const MButtonForm = ({ surs, children, ...props }) => {
     return (
         <button className={st.MButtonForm} {...props}>
             {children}
-            <img src={surs} alt='ButtonType'></img>
+            <img src={surs} alt=''></img>
         </button>
     );
 };
