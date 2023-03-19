af12ab5df907cbd8415816a35171e527fda956aa
lamalmao <laesia>
2023-03-19 15:41:00 +0300

    lfix

--- backend/src/models/game/geojson.ts
+++ backend/src/models/game/geojson.ts
@@ -1,4 +1,4 @@
-import { Schema, SchemaTypes } from 'mongoose';
+import { Schema } from 'mongoose';
 import { IFeature, IFeatureCollection } from './geojson.d.js';
 
 export const Feature = new Schema<IFeature>({
@@ -16,7 +16,7 @@ export const Feature = new Schema<IFeature>({
   geometry: {
     type: {
       coordinates: {
-        type: SchemaTypes.Array,
+        type: [],
         required: true
       },
       type: {
