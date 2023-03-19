fc2e2420c5d2e946510149137afa2c39bd602287
lamalmao <laesia>
2023-03-19 16:39:31 +0300

    возможный фикс проблемы со скрытыми играми

--- backend/src/controllers/games/get/index.ts
+++ backend/src/controllers/games/get/index.ts
@@ -34,10 +34,8 @@ export default async function getGameController(req: Request<{}, {}, IGameGetReq
       if (!user) {
         throw new Error('пользователь не найден');
       }
-
-      console.log(game.creator, userId);
-
-      if (game.creator !== userId && user.role !== ROLES.ADMIN) {
+      
+      if (game.creator != userId && user.role !== ROLES.ADMIN) {
         throw new Error('игра скрыта');
       }
     }
